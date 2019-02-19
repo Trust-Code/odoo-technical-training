748e165ba5318436f52fe4887bcbe96ed9fed654
Danimar Ribeiro <danimaribeiro@gmail.com>
2019-02-19 14:57:44 -0300

    [IMP] Validação através de constrains

--- school_management/models/school_class.py
+++ school_management/models/school_class.py
@@ -1,5 +1,6 @@
 
-from odoo import fields, models
+from odoo import api, fields, models
+from odoo.exceptions import ValidationError
 
 
 class SchoolClass(models.Model):
@@ -12,10 +13,16 @@ class SchoolClass(models.Model):
          ('wednesday', 'Wednesday'),
          ('thursday', 'Thursday'),
          ('friday', 'Friday Yey')], string="Week Day")
-    starting_hour = fields.Float(string="Starting Hour")
-    ending_hour = fields.Float(string="Ending Hour")
+    starting_hour = fields.Float(string="Starting Hour", required=True)
+    ending_hour = fields.Float(string="Ending Hour", required=True)
 
     partner_id = fields.Many2one('res.partner', string="Professor")
     hour_price = fields.Float(string="Price per Hour")
 
     total_price = fields.Float(string="Total Price")
+
+    @api.one
+    @api.constrains('starting_hour', 'ending_hour')
+    def _check_description(self):
+        if self.ending_hour <= self.starting_hour:
+            raise ValidationError("Hora Final deve ser maior que hora inicial")
