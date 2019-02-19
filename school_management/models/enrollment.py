6bfa84dd69277e14f205715d94b7961525f2d7ca
Danimar Ribeiro <danimaribeiro@gmail.com>
2019-02-19 16:26:33 -0300

    [IMP] Campos computados

--- school_management/models/enrollment.py
+++ school_management/models/enrollment.py
@@ -1,4 +1,5 @@
-from odoo import fields, models
+from odoo import api, fields, models
+from odoo.exceptions import ValidationError
 
 
 class Enrollment(models.Model):
@@ -6,11 +7,19 @@ class Enrollment(models.Model):
 
     partner_id = fields.Many2one('res.partner', string="Student")
 
-    start_date = fields.Date(string="Start Date")
-    end_date = fields.Date(string="End Date")
+    start_date = fields.Date(string="Start Date", required=True)
+    end_date = fields.Date(string="End Date", required=True)
 
     school_class_ids = fields.Many2many('school.class', string="Classes")
 
     gross_price = fields.Float(string="Base Price")
     discount = fields.Float(string="Discount (%)")
     net_price = fields.Float(string="Net Amount Monthly")
+
+    @api.one
+    @api.constrains('start_date', 'end_date')
+    def _check_date_intervals(self):
+        start = fields.Date.from_string(self.start_date)
+        end = fields.Date.from_string(self.end_date)
+        if start >= end:
+            raise ValidationError("Data final deve ser maior que data inicial")
--- school_management/models/school_class.py
+++ school_management/models/school_class.py
@@ -15,14 +15,27 @@ class SchoolClass(models.Model):
          ('friday', 'Friday Yey')], string="Week Day")
     starting_hour = fields.Float(string="Starting Hour", required=True)
     ending_hour = fields.Float(string="Ending Hour", required=True)
+    total_hours = fields.Float(
+        string="Total Hours", compute='_compute_total_hours', store=True)
 
     partner_id = fields.Many2one('res.partner', string="Professor")
     hour_price = fields.Float(string="Price per Hour")
 
-    total_price = fields.Float(string="Total Price")
+    total_price = fields.Float(
+        string="Total Price", compute='_compute_total_price', store=True)
 
     @api.one
     @api.constrains('starting_hour', 'ending_hour')
     def _check_description(self):
         if self.ending_hour <= self.starting_hour:
             raise ValidationError("Hora Final deve ser maior que hora inicial")
+
+    @api.depends('starting_hour', 'ending_hour')
+    def _compute_total_hours(self):
+        for item in self:
+            item.total_hours = item.ending_hour - item.starting_hour
+
+    @api.depends('total_hours', 'hour_price')
+    def _compute_total_price(self):
+        for item in self:
+            item.total_price = item.total_hours * item.hour_price
